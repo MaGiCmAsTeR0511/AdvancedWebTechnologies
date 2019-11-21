b2833be591217d735e3bce25345456b22f6645d2
MaGiCmAsTeR0511 <kurt.hohenauer@n.roteskreuz.at>
2019-11-21 12:35:04 +0100

    ne app

---
+++ 08 Real Time - PWA/Accounting/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SkillsListComponent } from './skills/skills-list/skills-list.component';
+import { SkillsEditComponent } from './skills/skills-edit/skills-edit.component';
+import { KpiBarComponent } from './kpi-bar/kpi-bar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkillsListComponent,
+    SkillsEditComponent,
+    KpiBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
